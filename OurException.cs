660fe9aa13f2e858bf64725c40ccdfbd62c8f2b2
user9 <user9@mail.ru>
2024-06-24 17:24:05 +0200

    Добавьте файлы проекта.

---
+++ OurException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mdk_0101__lab5_
+{
+    internal class OurException : Exception
+    {
+        public OurException(string?message ):base(message) { }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using mdk_0101__lab5_;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("год:");
+int year = int.Parse(Console.ReadLine());
+Console.WriteLine("месяц:");
+int month = int.Parse(Console.ReadLine());
+Console.WriteLine("день:");
+int day = int.Parse(Console.ReadLine());
+DateTime currentdate = new DateTime(year, month, day);
+DayOfWeek whatday = currentdate.DayOfWeek;
+Console.WriteLine($"дата{whatday}");
